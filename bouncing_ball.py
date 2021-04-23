f37823c793e4576208701c2834bc38e554a503ed
Paramesh Chandra <parameshchandra28@gmail.com>
2021-04-23 10:04:36 +0530

    update

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -1,7 +1,7 @@
 from vpython import *
 
 ball = sphere(
-    pos=vector(.1,0,0),
+    pos=vector(0,0,0),
     color = color.red,
     radius = 0.2,
     opacity = 0.6
@@ -11,10 +11,11 @@ thk = .2
 s2 = 2*side - thk
 s3 = 2*side + thk
 
+
 wallBK = box(pos=vector(0, 0, -side), size=vector(s2, s2, thk), color = color.gray(0.7))
 
 ball.mass = 1.0
-ball.p = vector(0,0,-1)
+ball.p = vector(1,0,0)
 g = vector(0,0,-10)
 dt = 0.00003
 time = 0.0
@@ -28,7 +29,7 @@ while True:
     ball.p.z = ball.mass*((ball.p.z/ball.mass) + g.z*time)
     ball.pos.z = ball.pos.z + (ball.p.z/ball.mass)*time
     ball.pos.x = ball.pos.x + ball.p.x*time 
-    if(ball.pos.z <= -side):
+    if(ball.pos.z < -side):
         ball.p.z = -ball.p.z
 
 
