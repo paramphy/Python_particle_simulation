50d1004e8c3aa870a3c003be6512b01c2377df34
Paramesh Chandra <parameshchandra28@gmail.com>
2021-04-28 17:07:42 +0530

    update

---
+++ inclined_plane.py
@@ -0,0 +1,22 @@
+from vpython import *
+
+
+
+hight = 5
+
+g = vector(0,0,-10)
+
+theta = pi/3
+
+longest = box(pos=vector(0,0,0),size=vector(0.3,hight,hight*sin(theta)), color=color.red)
+
+longest.rotate(angle=pi/3,axis=vector(0,0,1))
+
+ball = sphere(
+    pos=vector(0,hight,hight*sin(theta)),
+    color = color.red,
+    radius = 0.3,
+    opacity = 0.6,
+    make_trail=True,
+    retain=200
+)
